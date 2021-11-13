02f1c445e4f4381d8dac1b0528c8acbb383b99ef
Milton Riscanevo <miltonriscanevo@hotmail.com>
2021-11-12 23:07:35 -0500

    adicionando procesos

--- src/components/datarequirements/ProcesoDeRequerimientos.jsx
+++ src/components/datarequirements/ProcesoDeRequerimientos.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const ProcesoDeRequerimientos = () => {
     return (
         <div>
-            aqui va el proceso
+            <h2>Como son los procesos</h2>
+            <p>En la ingeniería de software se usan los procesos de requisitos y este tiene distintas partes y estas son:</p>
+
+        <p> <strong>Actores del proceso:</strong> <br />  Este es un proceso interdisciplinado que hace de mediador entre la ingeniería de software y el dominio del interesado, a menudo este proceso tiene muchas personas involucradas y cada una de ellas tiene un interés en el software y el interés varía según el proyecto, pero siempre va a ver usuarios / operadores y clientes (que casi todo el tiempo no son los mismos. <br /> Aquí un ejemplo práctico de los interesados en software (no contiene todas las partes que puedan estar interesadas)
+           <br />
+           <br /><strong>-Usuarios:</strong>  Este grupo es uno de los que solo se enfoca en la utilización del software, este grupo involucra a personas con diferentes roles.
+           <br />
+           <br /> <strong>-Clientes:</strong>  Este grupo representa a las personas que compraron o encargaron el software y representan el mercado adjetivo del software.
+           <br />
+           <br /> <strong>-Analistas de mercado:</strong> Este grupo se enfoca en el marketing para poder promocionar el producto en el mercado masivo.
+           <br />
+           <br /> <strong>-Reguladores:</strong> Este grupo se enfoca en la regulación de las aplicaciones de uso cotidiano por los ciudadanos.
+           <br />
+           <br /> <strong>-Ingenieros de software:</strong> Este grupo está conformado por personas que están real mente interesadas por el desarrollo de software, en la mayoría de los casos reutilizan parte de otros proyectos, pero si un cliente pide unas especificaciones que impidan la reutilización de este los ingenieros deberán tener mucho cuidado de su participación con el cliente ya que los requerimientos específicos y las limitaciones puede tener un gran impacto en la producción del proyecto 
+           <br />
+           <br /> <strong>Gestión y soporte de procesos:</strong> <br /> En esta sección se presentas los recursos de gestión que se usaron de proyectos requeridos y consumidos por el proceso de requisitos. El propósito de este es establecer un vínculo entre las actividades del proceso identificadas 2.1 y los temas de costos, recursos humanos, capacitación y herramientas 
+           <br />
+           <br /> <strong>Calidad y mejora de los procesos:</strong> <br />  Este teme está relacionado con la evaluación de los procesos de requisitos. El propósito de este es de jugar un papel clave en los procesos de requerimientos en términos de costo, oportunidades de software y de la satisfacción de del cliente con el producto. Este ayuda mucho al proceso de requerimientos con estándares de calidad y modelos de mejora con los procesos del software y sistemas, la mejora del proceso está relacionada con la calidad del software KA.</p>    
+
         </div>
     )
 }
